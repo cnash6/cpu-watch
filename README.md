a6dc9090bed2e1839585666e1a82c739a5de3273
Christian Nash <cnash3712@gmail.com>
2020-12-13 12:50:16 -0500

    v 0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# CPU Watch
+
+I want a new CPU. Now I just need to know when they're in stock
---
+++ cpuwatch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import requests
+import schedule
+import time
+import json
+import boto3
+
+def get_stock():
+    items = json.loads(open('items.json', 'r').read())
+    changed = []
+    for i, item in enumerate(items['items']):
+        stock = BeautifulSoup(requests.get(item['url']).text, 'html.parser').find_all("span", class_="inventoryCnt")[0].string
+        if stock != item['stock']:
+            upT = time.strftime("%m/%d %H:%M:%S", time.localtime())
+            changed.append(f'{item["item"]} updated from:\n\t{items["items"][i]["stock"]} => {stock}\n\t@ {upT}')
+            items['items'][i]['stock'] = stock
+    
+
+
+    if changed:
+        with open('items.json', 'w') as f:
+            f.write(json.dumps(items))
+
+        message = f'Inventory Updates:\n\n{chr(10).join(changed)}'
+        print(message)
+        boto3.client('sns').publish(
+            PhoneNumber=items['number'],
+            Message=message
+        )
+
+
+schedule.every(5).minutes.at(":00").do(get_stock)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
