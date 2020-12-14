25b7adc19b4c87312cfa979b43d8b11e7c5b4eab
Christian Nash <cnash3712@gmail.com>
2020-12-14 10:59:38 -0500

    better logging

--- cpuwatch.py
+++ cpuwatch.py
@@ -10,26 +10,27 @@ import boto3
 def get_stock():
     items = json.loads(open('items.json', 'r').read())
     changed = []
-    for i, item in enumerate(items['items']):
-        stock = BeautifulSoup(requests.get(item['url']).text, 'html.parser').find_all("span", class_=item['tag'])[0].string
-        if stock != item['stock']:
-            upT = time.strftime("%m/%d %H:%M:%S", time.localtime())
-            changed.append(f'{item["item"]} updated from:\n\t{items["items"][i]["stock"]} => {stock}\n\t@ {upT}')
-            items['items'][i]['stock'] = stock
-    
+    upT = time.strftime("%m/%d %H:%M:%S", time.localtime())
+    try:
+        for i, item in enumerate(items['items']):
+            stock = BeautifulSoup(requests.get(item['url']).text, 'html.parser').find_all("span", class_=item['tag'])[0].string
+            if stock != item['stock']:
+                changed.append(f'{item["item"]} updated from:\n\t{items["items"][i]["stock"]} => {stock}\n\t@ {upT}')
+                items['items'][i]['stock'] = stock
+                items['items'][i]['updated'] = upT
+        print(f'checked at {upT}')
+        if changed:
+            with open('items.json', 'w') as f:
+                f.write(json.dumps(items))
 
-
-    if changed:
-        with open('items.json', 'w') as f:
-            f.write(json.dumps(items))
-
-        message = f'Inventory Updates:\n\n{chr(10).join(changed)}'
-        print(message)
-        boto3.client('sns').publish(
-            PhoneNumber=items['number'],
-            Message=message
-        )
-
+            message = f'Inventory Updates:\n\n{chr(10).join(changed)}'
+            print(message)
+            boto3.client('sns').publish(
+                PhoneNumber=items['number'],
+                Message=message
+            )
+    except:
+        print(f'Encountered exception fetching prices at {upT}')
 
 schedule.every(10).minutes.at(":00").do(get_stock)
 
